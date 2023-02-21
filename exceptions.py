c67bdfef5a9e03bed58c342df2cda995a6239b80
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-02-22 01:00:56 +0300

    22222222
    222222222222222

---
+++ exceptions.py
@@ -0,0 +1,15 @@
+class HomeworkApiError(Exception):
+    pass
+
+
+class APIStatusCodeError(Exception):
+    pass
+
+
+class EndPointError(Exception):
+    pass
+
+
+class StatusTypeError(Exception):
+    pass
+
