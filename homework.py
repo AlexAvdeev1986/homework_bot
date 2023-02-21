58a536d56338c50b4dd2f110fb63a1edf489c3c9
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-02-22 01:14:54 +0300

    333333333
    33333333333333333

--- homework.py
+++ homework.py
@@ -22,7 +22,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 300
+RETRY_PERIOD = 300
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 URL = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -159,7 +159,7 @@ def main():
             send_message(bot, error)
 
         finally:
-            time.sleep(RETRY_TIME)
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
