91cb843544dbcf4227eb346d1eee9b7494a9bd5f
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-02-26 20:14:03 +0300

    4444444444444
    4555555555555555

--- homework.py
+++ homework.py
@@ -44,7 +44,7 @@ def check_tokens() -> None:
     Райзит исключение при потере какого-либо токена.
     """
     tokens = all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID])
-    return tokens or logging.critical(c.TOKEN_NOT_FOUND.format(tokens))
+    return tokens or logging.critical(TOKEN_NOT_FOUND.format(tokens))
 
 
 def send_message(bot: telegram.Bot, text: str) -> None:
