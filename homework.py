fdeb36cabae532a83bacf1699b110c8a8e82107e
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-03-04 01:56:58 +0300

    88
    8

--- homework.py
+++ homework.py
@@ -9,34 +9,42 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-from exceptions import (EndpointFailureResponseCodes, InvalidTokens,
-                        ResponseFormatFailure, WrongStatusInResponse,CustomException)
+from exceptions import (
+    EndpointFailureResponseCodes,
+    InvalidTokens,
+    ResponseFormatFailure,
+    WrongStatusInResponse,
+    CustomException,
+)
 
 
 load_dotenv()
 BASE_DIR = os.path.dirname(__file__)
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 RETRY_PERIOD = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
+HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 LAST_DAY_OFFSET = 86400
 HOMEWORK_VERDICTS = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
 logger = logging.getLogger(__name__)
 handler = logging.StreamHandler(stream=sys.stdout)
 file_handler = RotatingFileHandler(
-    os.path.join(BASE_DIR, 'homework_bot.log'), maxBytes=1000000,
-    backupCount=5, encoding='utf-8')
+    os.path.join(BASE_DIR, "homework_bot.log"),
+    maxBytes=1000000,
+    backupCount=5,
+    encoding="utf-8",
+)
 logger.addHandler(handler)
 logger.addHandler(file_handler)
 formatter = logging.Formatter(
-    '%(asctime)s [%(levelname)s] [%(filename)s]:[%(lineno)s] [%(funcName)s] '
-    '%(message)s '
+    "%(asctime)s [%(levelname)s] [%(filename)s]:[%(lineno)s] [%(funcName)s] "
+    "%(message)s "
 )
 handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
@@ -49,31 +57,28 @@ def check_tokens():
 
 def send_message(bot, message):
     """Send status update."""
-    logger.info(f'Попытка отправить сообщение \"{message}\" в чат бота')
+    logger.info(f'Попытка отправить сообщение "{message}" в чат бота')
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except telegram.error.TelegramError as error:
-        logger.error(f'Ошибка отправки сообщения в чат бота - {error}')
+        logger.error(f"Ошибка отправки сообщения в чат бота - {error}")
         return False
     else:
         logger.debug(
-            f'Message \"{message}\" was sent from bot to chat '
-            f'{TELEGRAM_CHAT_ID}')
+            f'Message "{message}" was sent from bot to chat '
+            f"{TELEGRAM_CHAT_ID}"
+        )
         return True
 
 
 def get_api_answer(timestamp):
     """Yandex API answer retrieval function."""
-    params = {'from_date': timestamp}
-    request_params = {
-        'url': ENDPOINT,
-        'headers': HEADERS,
-        'params': params
-    }
+    params = {"from_date": timestamp}
+    request_params = {"url": ENDPOINT, "headers": HEADERS, "params": params}
     logger.info(
         (
-            'Начинаем подключение к эндпоинту {url}, с параметрами'
-            ' headers = {headers} ;params= {params}.'
+            "Начинаем подключение к эндпоинту {url}, с параметрами"
+            " headers = {headers} ;params= {params}."
         ).format(**request_params)
     )
 
@@ -81,47 +86,53 @@ def get_api_answer(timestamp):
         homework_statuses = requests.get(**request_params)
         if homework_statuses.status_code != http.HTTPStatus.OK:
             raise EndpointFailureResponseCodes(
-                f'Please check why API response failed = '
-                f'status_code:{homework_statuses.status_code}, '
-                f'reason:{homework_statuses.reason}, '
-                f'text: {homework_statuses.text}')
+                f"Please check why API response failed = "
+                f"status_code:{homework_statuses.status_code}, "
+                f"reason:{homework_statuses.reason}, "
+                f"text: {homework_statuses.text}"
+            )
         return homework_statuses.json()
     except Exception as error:
         raise ConnectionError(
             f'Ошибка "{error}" соединения при попыткы запроса  '
-            'статуса домашней работы c параметрами: '
-            'url = {url}, '
-            'headers = {headers}, '
-            'params = {params}, '.format(**request_params))
+            "статуса домашней работы c параметрами: "
+            "url = {url}, "
+            "headers = {headers}, "
+            "params = {params}, ".format(**request_params)
+        )
 
 
 def check_response(response):
     """API response validation based on documentation."""
     if not isinstance(response, dict):
-        raise TypeError('Response data format is not dictionary')
-    if 'homeworks' not in response:
+        raise TypeError("Response data format is not dictionary")
+    if "homeworks" not in response:
         raise ResponseFormatFailure(
-            'Please check homeworks existence in response')
-    if 'current_date' not in response:
+            "Please check homeworks existence in response"
+        )
+    if "current_date" not in response:
         raise ResponseFormatFailure(
-            'Please check current_date existence in response')
-    homeworks = response.get('homeworks')
+            "Please check current_date existence in response"
+        )
+    homeworks = response.get("homeworks")
     if not isinstance(homeworks, list):
-        raise TypeError('Response data format is not list')
+        raise TypeError("Response data format is not list")
     return homeworks
 
 
 def parse_status(homework):
     """Check homework status from API response."""
-    status = homework.get('status')
+    status = homework.get("status")
     if status not in HOMEWORK_VERDICTS:
         raise WrongStatusInResponse(
             f'Please validate status - "{status}" in response is based on '
-            'yandex documentation')
-    if 'homework_name' not in homework:
+            "yandex documentation"
+        )
+    if "homework_name" not in homework:
         raise ResponseFormatFailure(
-            'Please validate homework_name exist in response')
-    homework_name = homework.get('homework_name')
+            "Please validate homework_name exist in response"
+        )
+    homework_name = homework.get("homework_name")
     verdict = HOMEWORK_VERDICTS[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -129,14 +140,14 @@ def parse_status(homework):
 def main():
     """Yandex-practicum homework status changes telegram notification."""
     if not check_tokens():
-        logger.critical('Пожалуйста, проверьте переменные окружения')
+        logger.critical("Пожалуйста, проверьте переменные окружения")
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = 0
     current_report = {
-        'message_output': '',
+        "message_output": "",
     }
     prev_report = {
-        'message_output': '',
+        "message_output": "",
     }
 
     while True:
@@ -145,30 +156,31 @@ def main():
             homeworks_list = check_response(response)
             if homeworks_list:
                 last_homework_name = homeworks_list[0]
-                current_report['message_output'] = parse_status(
-                    last_homework_name)
+                current_report["message_output"] = parse_status(
+                    last_homework_name
+                )
             else:
-                current_report['message_output'] = 'Обновлений нет'
+                current_report["message_output"] = "Обновлений нет"
             if current_report != prev_report:
-                if send_message(bot, current_report['message_output']):
+                if send_message(bot, current_report["message_output"]):
                     prev_report = current_report.copy()
-                    timestamp = response.get('current_date', timestamp)
+                    timestamp = response.get("current_date", timestamp)
             else:
-                logger.debug('Обновлений нет')
+                logger.debug("Обновлений нет")
         except InvalidTokens as e:
-                logger.error(str(e))
+            logger.error(str(e))
         except ResponseFormatFailure as error:
             logger.error(error)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            current_report['message_output'] = message
+            message = f"Сбой в работе программы: {error}"
+            current_report["message_output"] = message
             logger.error(message, exc_info=True)
             if current_report != prev_report:
-                send_message(bot, current_report['message_output'])
+                send_message(bot, current_report["message_output"])
                 prev_report = current_report.copy()
         finally:
             time.sleep(RETRY_PERIOD)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
