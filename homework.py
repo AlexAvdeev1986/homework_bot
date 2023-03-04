238cf1de60bc51ff235d3aa9b34a5681736d6dcc
AlexAvdeev1986 <116315897+AlexAvdeev1986@users.noreply.github.com>
2023-03-04 21:26:46 +0300

    5
    55

--- homework.py
+++ homework.py
@@ -14,7 +14,7 @@ from exceptions import (
     InvalidTokens,
     ResponseFormatFailure,
     WrongStatusInResponse,
-    Errorsendingmessage,
+    CustomException,
 )
 
 
@@ -61,7 +61,7 @@ def send_message(bot, message):
         bot.send_message(TELEGRAM_CHAT_ID, message)
     except telegram.error.TelegramError as error:
         logger.error(f"Ошибка отправки сообщения в чат бота - {error}")
-        raise Errorsendingmessage(
+        raise CustomException(
             "Ошибка отправки сообщения в чат бота"
         ) from error
     else:
@@ -171,7 +171,7 @@ def main():
             else:
                 logger.debug("Обновлений нет")
                 send_message(bot, message)
-        except Errorsendingmessage as error:
+        except CustomException as error:
             logger.error(f"Ошибка в отправке сообщения - {error}")
         except Exception as error:
             message = f"Сбой в работе программы: {error}"
